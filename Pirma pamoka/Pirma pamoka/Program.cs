a5d4444d70f82d564f111a1de78cbd5879a1b8d8
Donatas Vitkus <vitk.donatas@gmail.com>
2022-01-02 22:48:57 +0200

    Created new application

--- Pirma pamoka/Pirma pamoka/Program.cs
+++ Pirma pamoka/Pirma pamoka/Program.cs
@@ -6,7 +6,82 @@ namespace Pirma_pamoka
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1 UZDUOTIS Swap values between two variables.
+
+            //int a = 7, b = 9;
+            //Console.WriteLine("Before swap a= " + a + " b= " + b);
+            //a = a * b; //a=63 (7*9)      
+            //b = a / b; //b=7 (63/9)      
+            //a = a / b; //a=9 (63/7)    
+            //Console.Write("After swap a= " + a + " b= " + b);
+
+            // 2 UZDUOTIS  Take 2 input variables from console and output the result of ‘==’ of them (true/false).
+
+            //int x;
+            //int y;
+            //String TF = "";
+
+            //Console.WriteLine("Enter first digit");
+            //x = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter second digit");
+            //y = Convert.ToInt32(Console.ReadLine());
+
+            //bool result = x==y;
+            //Console.WriteLine(x == y);
+
+            // 3 UZDUOTIS Take 2 input variables from console then add 1 to the first one and subtract 1 from the second one.
+
+            //int x;
+            //int y;
+            //String TF = "";
+
+            //Console.WriteLine("Enter first digit");
+            //x = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter second digit");
+            //y = Convert.ToInt32(Console.ReadLine());
+            //x = ++x;
+            //y = --y;
+            //Console.Write($"After add & subtract x= {x} y= {y}");
+
+            // 4 UZDUOTIS Calculate rectangle perimeter and area.
+
+            //int l, w, area, perimeter;
+            //Console.Write("Length : ");
+            //l = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Width : ");
+            //w = Convert.ToInt32(Console.ReadLine());
+            //area = l * w;
+            //perimeter = 2 * (l + w);
+            //Console.WriteLine("Area of Rectangle : " + area);
+            //Console.WriteLine("Perimeter of Rectangle : " + perimeter);
+            //Console.ReadKey();
+
+            //5 UZDUOTIS
+
+            //6 UZDUOTIS Check if a number is lesser than 0.
+
+            //Console.WriteLine("Please write your number:");
+            //int n = int.Parse(Console.ReadLine());
+
+            //if ((n > 0))
+            //{
+            //    Console.WriteLine("True");
+            //}
+            //else
+            //{
+            //   Console.WriteLine("False");
+            //}
+
+            //7 UZDUOTIS Take name and lastname as separate inputs and output them in one line.
+
+            //string name;
+            //string lastname;
+
+            //Console.WriteLine("Enter your name");
+            //name = Console.ReadLine();
+            //Console.WriteLine("Enter your last name");
+            //lastname = Console.ReadLine();
+            //Console.WriteLine($"{name} {lastname}");
         }
     }
 }
